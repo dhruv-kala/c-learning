33455d039bd16a9688b92bb5e5ca2b876d72094c
Niranjan Singh <niranjan.kala@gmail.com>
2024-06-02 12:30:00 +0530

    Added first program

---
+++ add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    printf("Enter number a: ");
+    scanf("%d", &a);
+    printf("Enter number b: ");
+    scanf("%d", &b);
+
+    printf("This sum is %d\n",a+b);
+
+    return 0;
+}
