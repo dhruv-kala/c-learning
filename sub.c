40050f6a0b7a6956e29ac7db356c0b12d4a109ca
Dhruv Kumar <dhruvk.kala@gmail.com>
2024-06-02 13:07:26 +0530

    Added subtration program

---
+++ sub.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter number a: ");
+    scanf("%d", &a);
+
+    printf("Enter number b: ");
+    scanf("%d", &b);
+
+    printf("Subtraction of a and b is %d\n", a - b);
+
+    return 0;
+}
