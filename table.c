a97aab517d10eff777a28bbd86eb2c194d041052
Dhruv Kumar <dhruvk.kala@gmail.com>
2024-06-06 11:58:25 +0530

    edit program for tables

---
+++ table.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("enter a number you want multiplication of");
+    scanf("%d", &a);
+
+    printf("%dx1=%d\n", a, a * 1);
+    printf("%dx2=%d\n", a, a * 2);
+    printf("%dx3=%d\n", a, a * 3);
+    printf("%dx4=%d\n", a, a * 4);
+    printf("%dx5=%d\n", a, a * 5);
+    printf("%dx6=%d\n", a, a * 6);
+    printf("%dx7=%d\n", a, a * 7);
+    printf("%dx8=%d\n", a, a * 8);
+    printf("%dx9=%d\n", a, a * 9);
+    printf("%dx10=%d\n", a, a * 10);
+
+    return 0;
+}
