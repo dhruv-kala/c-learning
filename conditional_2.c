4859d2cdb5ea25e402f8dde307ff3633018d478c
Dhruv Kumar <dhruvk.kala@gmail.com>
2024-06-25 11:19:35 +0530

    if and echo entered name program

---
+++ conditional_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+
+    printf("Enter a number");
+    scanf("%d", &a);
+
+    if (a % 2 == 0)
+    {
+        printf("the number is even");
+    }
+    else
+    {
+        printf("the number is odd");
+    }
+}
---
+++ displayname.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char name[50] ;
+
+    printf ("Enter your name:\n");
+    scanf("%s",&name);
+    printf("Hello! %s", name );
+
+}
